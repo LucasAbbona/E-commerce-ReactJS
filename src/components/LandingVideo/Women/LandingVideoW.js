36eb540a3752f0d90169e76ccb8c800f4e228c4e
Lucas <lucasabbona0@gmail.com>
2023-01-15 17:54:17 -0300

    PreEntrega2-Lucas Abbona

--- src/components/LandingVideo/Women/LandingVideoW.js
+++ src/components/LandingVideo/Women/LandingVideoW.js
@@ -6,7 +6,7 @@ const LandingVideoW = () => {
 
   return (
         <div className='VideoBlur'>
-        <video src={AnuncioW} autoPlay className='Video' poster="https://s01.sgp1.cdn.digitaloceanspaces.com/article/126864-zfwymgwgcj-1567691366.jpeg" loop controlsList="nodownload noremoteplayback" disableremoteplayback muted></video>
+        <video src={AnuncioW} autoPlay className='Video' poster="https://www.numeromag.nl/wp-content/uploads/2022/04/chanel_chanel-22-bag_ad-campaign-by-inez-and-vinoodh-1-HD-1024x668.jpg" loop controlsList="nodownload noremoteplayback" disableremoteplayback muted></video>
         <div className='BackVideo'></div>
         <Link to={"/category/women"}><button className='Women'>Discover Women</button></Link>
         
